557ddbf0355baf4dc500670c6d536d1e9464b9a8
Elkami_kaze <mickael.guillot@etud.univ-angers.fr>
2012-10-03 11:42:03 +0200

    ajout du projet

---
+++ build/CMakeFiles/projet_commun.dir/DependInfo.cmake
@@ -0,0 +1,14 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/etudiant/Dropbox/Cours/M1/Projet_commun/SOURCES/Projet_Commun/main.cpp" "/home/etudiant/Dropbox/Cours/M1/Projet_commun/SOURCES/Projet_Commun/build/CMakeFiles/projet_commun.dir/main.cpp.o"
+  "/home/etudiant/Dropbox/Cours/M1/Projet_commun/SOURCES/Projet_Commun/test.cpp" "/home/etudiant/Dropbox/Cours/M1/Projet_commun/SOURCES/Projet_Commun/build/CMakeFiles/projet_commun.dir/test.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ build/CMakeFiles/projet_commun.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/projet_commun.dir/test.cpp.o"
+  "CMakeFiles/projet_commun.dir/main.cpp.o"
+  "projet_commun.pdb"
+  "projet_commun"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/projet_commun.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
